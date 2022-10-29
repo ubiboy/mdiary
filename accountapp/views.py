cf8da95784bc640a1a47d4024e443ce83b1d46ed
Zung Chad <zungziho@gmail.com>
2022-10-30 01:00:44 +0900

    django course 16 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context= {})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={})
+
