e0586414ce7616add28a0491af6f2f5d6c4c6288
Zung Chad <zungziho@gmail.com>
2022-10-30 20:20:02 +0900

    Bug Fix

--- mdiary/settings.py
+++ mdiary/settings.py
@@ -138,8 +138,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-LOGIN_REDIRECT_URL = reverse_lazy('account:hello_world')
-LOGIN_REDIRECT_URL = reverse_lazy('account:login')
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
