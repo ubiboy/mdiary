504f5d252cff88d1be808422488afc5bcbfea77b
Zung Chad <zungziho@gmail.com>
2022-10-30 21:07:06 +0900

    django course 29 commit

--- mdiary/settings.py
+++ mdiary/settings.py
@@ -145,3 +145,7 @@ LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
