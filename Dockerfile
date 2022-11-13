7b4bd6a52d42f042abd989f0160243ea34ba56d9
Zung Chad <zungziho@gmail.com>
2022-11-13 16:28:32 +0900

    articlecreate

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.9.0
 
 WORKDIR /home/
 
-RUN echo "testing123"
+RUN echo "testing12345"
 
 RUN git clone https://github.com/ubiboy/mdiary.git
 
--- articleapp/forms.py
+++ articleapp/forms.py
@@ -7,8 +7,10 @@ from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
-                                                           'style': 'height: auto; text-align:left'}))
+    # content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+    #                                                        'style': 'height: auto; text-align:left'}))
+    content = forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'height: auto; text-align:left'})
     project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
 
     class Meta:
