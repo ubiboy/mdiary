eda074a49871687828377b0c33189e7d5c8a4d80
Zung Chad <zungziho@gmail.com>
2022-11-17 13:35:03 +0900

    update

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -10,7 +10,7 @@ class ArticleCreationForm(ModelForm):
     # content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
     #                                                        'style': 'height: auto; text-align:left'}))
     content = forms.Textarea(attrs={'class': 'editable',
-                                                           'style': 'height: auto; text-align:left'})
+                                    'style': 'height: auto; text-align:left'})
     project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
 
     class Meta:
